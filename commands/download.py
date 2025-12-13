31996d798a9f1c23707525748569a489cb56954b
AlexNMD <alexandre.normand@icloud.com>
2025-12-13 12:14:07 +0100

    Fix default value

--- commands/download.py
+++ commands/download.py
@@ -13,21 +13,27 @@ class Download(commands.Cog):
         """USAGE: send link to download file (separate w/ ',')"""
 
         message = ctx.message
+        message_content = message.content.split()
+        links, type_dl = str(), None
 
-        _message_content = message.content.split()
-        match len(_message_content):
+        match len(message_content):
             case 2:
-                _, _links = _message_content
+                _, links = message_content
             case 3:
-                _, type_dl, _links = _message_content
+                _, type_dl, links = message_content
             case _:
-                await message.reply("USAGE: send link to download file (separate w/ ',')")
+                await message.reply(
+                    "USAGE: send link to download file (separate w/ ',')"
+                )
                 return
 
         try:
-            for url in _links.split(","):
+            for url in links.split(","):
                 task = download_task.delay(
-                    url=url, message_id=message.id, channel_id=message.channel.id, type_dl=type_dl
+                    url=url,
+                    message_id=message.id,
+                    channel_id=message.channel.id,
+                    type_dl=type_dl,
                 )
                 logger.info(f"Task sent: {task.id}")
         except Exception as download_error:
