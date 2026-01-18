3c869abf8926ead850e192bf321c3e115cdcf550
AlexNMD <me@alex-nmd.net>
2026-01-18 16:12:26 +0100

    add generic error handler

--- nmdownloader/apps/flask_app.py
+++ nmdownloader/apps/flask_app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from werkzeug.exceptions import HTTPException
 
 from nmdownloader.routes import register_routes
 
-flask_app = Flask(
-    __name__,
-    template_folder="../templates",
-    static_folder="../static"
-)
+flask_app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
 register_routes(flask_app)
+
+
+@flask_app.errorhandler(Exception)
+def handle_exception(error: Exception):
+    if isinstance(error, HTTPException):
+        return {"message": error.description}, error.code
+    else:
+        return {"message": str(error)}, 500
