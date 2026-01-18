20e5440e82de4a9980d73ca15cbb55c82a6bd9f8
AlexNMD <me@alex-nmd.net>
2026-01-18 13:15:46 +0100

    improve UI experience

--- nmdownloader/static/js/download.js
+++ nmdownloader/static/js/download.js
@@ -1,3 +1,59 @@
+const urlsField = document.getElementById('urls');
+const typeDlField = document.querySelector('input[name="option"]:checked');
+const alert = document.getElementById('urls-invalid');
+
+
+document.getElementById('downloadForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const urlsText = urlsField.value.trim();
+    const typeDl = typeDlField ? typeDlField.value : null
+
+    if (!urlsText) {
+        notification('Send one url at least', false)
+        return;
+    }
+
+    const urls = urlsText.split('\n').filter(url => url.trim());
+    const hasValidUrls = urls.every(url => isValidHttpUrl(url));
+    if (!hasValidUrls) {
+        alert.style.display = 'block';
+        return;
+    } else {
+        alert.style.display = 'none';
+    }
+
+    try {
+        const response = await fetch('/download', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                urls: urls,
+                type_dl: typeDl
+            })
+        });
+
+        if (!response.ok) {
+            const data = await response.json();
+            notification(`HTTP Error: ${response.status}. Message: ${data.message}`, false)
+            return
+        }
+
+        const data = await response.json();
+
+        notification(`${data["uuids"].length} download(s) started`);
+        urlsField.value = '';
+        if (typeDlField) typeDlField.checked = false
+
+    } catch (error) {
+        notification(`Error: ${error.message}`, false);
+        console.error('Error:', error);
+    }
+});
+
+
 function notification(text, success= true) {
     const container = document.getElementById("toast-container");
     const toast = document.createElement("div");
@@ -24,49 +80,14 @@ function notification(text, success= true) {
     setTimeout(() => toast.remove(), 2000);
 }
 
-document.getElementById('downloadForm').addEventListener('submit', async (e) => {
-    e.preventDefault();
-
-    const urlsField = document.getElementById('urls')
-    const typeDlField = document.querySelector('input[name="option"]:checked');
-
-    const urlsText = urlsField.value.trim();
-    const typeDl = typeDlField ? typeDlField.value : null
-
-    if (!urlsText) {
-        notification('Send one url at least', false)
-        return;
-    }
-
-    const urls = urlsText.split('\n').filter(url => url.trim());
+function isValidHttpUrl(string) {
+    let url;
 
     try {
-        const response = await fetch('/download', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                urls: urls,
-                type_dl: typeDl
-            })
-        });
-
-        if (!response.ok) {
-            const data = await response.json();
-            notification(`HTTP Error: ${response.status}. Message: ${data.message}`, false)
-            return
-        }
-
-        const data = await response.json();
-
-        notification(`${data["uuids"].length} download(s) started`);
-
-        urlsField.value = '';
-        if (typeDlField) typeDlField.checked = false
-
-    } catch (error) {
-        notification(`Error: ${error.message}`, false);
-        console.error('Error:', error);
+        url = new URL(string);
+    } catch (_) {
+        return false;
     }
-});
+
+    return url.protocol === "http:" || url.protocol === "https:";
+}
