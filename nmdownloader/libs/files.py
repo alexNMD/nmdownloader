99aab1f194d8b931ee5040d3aad5e0b05ded8350
AlexNMD <me@alex-nmd.net>
2026-01-19 22:40:37 +0100

    Add regex for fallback case

--- nmdownloader/libs/files.py
+++ nmdownloader/libs/files.py
@@ -2,19 +2,24 @@ import re
 from pathlib import Path
 
 SERIE_REGEX = r"^(?P<name>.+?)[\s._]*[Ss](?P<season>\d+)(?:[Ee](?P<episode>\d+))?"
+SERIE_REGEX_FALLBACK = r"^(?P<name>(?:[A-Z][a-z]*\.?)+[A-Z][a-z]*)"
 
 
-def _extract_serie_info(filename: str) -> dict | None:
-    if not (match := re.search(SERIE_REGEX, filename)):
-        return None
+def _extract_serie_info(filename: Path) -> dict:
+    _filename_str = str(filename)
+    if match := re.search(SERIE_REGEX, _filename_str):
+        return match.groupdict()
+    if fallback_match := re.search(SERIE_REGEX_FALLBACK, _filename_str):
+        return fallback_match.groupdict()
 
-    return match.groupdict()
+    return {"name": filename.stem}
 
 
-def get_relative_directory(filename: str) -> Path | None:
-    if not (serie_info := _extract_serie_info(filename)):
-        return None
-
+def get_relative_directory(filename: Path) -> Path:
+    serie_info = _extract_serie_info(filename)
     serie_name = Path(serie_info["name"])
 
-    return serie_name / f"Saison.{serie_info.get('season', '1')}"
+    if season := serie_info.get("season"):
+        return serie_name / f"Saison.{season}"
+
+    return serie_name
