5941a1ae13f70bf3742f00f92e5260111773e34c
AlexNMD <me@alex-nmd.net>
2026-03-23 17:04:27 +0100

    Merge remote-tracking branch 'origin/chore/separate-config-base-and-plugins' into chore/separate-config-base-and-plugins

--- nmdownloader/config/base.py
+++ nmdownloader/config/base.py
@@ -5,6 +5,7 @@ from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from nmdownloader.config.plugins.un_fichier import UnFichierDownloaderConfig
+from nmdownloader.config.plugins.youtube import YoutubeDownloaderConfig
 
 
 class DiscordConfig(BaseSettings):
@@ -42,40 +43,7 @@ class DownloaderConfig(BaseSettings):
     un_fichier: UnFichierDownloaderConfig = Field(
         default_factory=UnFichierDownloaderConfig
     )
-
-
-class FFMPEGVideoConfig(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_prefix="FFMPEG_",
-        env_file=".env",
-        env_file_encoding="utf-8",
-        extra="ignore",
-    )
-    vcodec: str = "libx264"
-    crf: int = Field(default=30, ge=0, le=51)
-    preset: str = Field(
-        default="ultrafast",
-        pattern="^(ultrafast|superfast|veryfast|faster|fast|medium|slow|slower|veryslow)$",
-    )
-    profile_v: str = Field(default="baseline", alias="profile:v")
-    tune: str = "fastdecode"
-    acodec: str = "aac"
-    audio_bitrate: str = Field(default="96k", alias="b:a")
-    movflags: str = "+faststart"
-    format: str = "mp4"
-
-    def to_dict(self) -> dict:
-        return {
-            "vcodec": self.vcodec,
-            "crf": self.crf,
-            "preset": self.preset,
-            "profile:v": self.profile_v,
-            "tune": self.tune,
-            "acodec": self.acodec,
-            "b:a": self.audio_bitrate,
-            "movflags": self.movflags,
-            "format": self.format,
-        }
+    youtube: YoutubeDownloaderConfig = Field(default_factory=YoutubeDownloaderConfig)
 
 
 class Settings(BaseSettings):
@@ -86,7 +54,6 @@ class Settings(BaseSettings):
     discord: DiscordConfig = Field(default_factory=DiscordConfig)
     celery: CeleryConfig = Field(default_factory=CeleryConfig)
     downloader: DownloaderConfig = Field(default_factory=DownloaderConfig)
-    ffmpeg: FFMPEGVideoConfig = Field(default_factory=FFMPEGVideoConfig)
 
     media_path: Path = Field(default=Path("/media"))
     nmd_log_level: str = Field(default="INFO")
