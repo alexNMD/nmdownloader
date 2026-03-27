c054371c291b41ba155df4428bccfc21df4c5687
Alexandre <me@alex-nmd.net>
2026-03-27 11:49:04 +0100

    Update Discord bot to use new Intents structure
    Refactor Discord bot initialization to use updated Intents import and remove redundant main function.

--- nmdownloader/apps/discord_app.py
+++ nmdownloader/apps/discord_app.py
@@ -1,26 +1,20 @@
-import discord
+from discord import Intents
 
-from nmdownloader.config.base import app_settings
+from nmdownloader.config import app_settings
 from nmdownloader.services.bot import NMDownloader
 
-custom_intents = discord.Intents.default()
-custom_intents.typing = False
-custom_intents.presences = False
-custom_intents.message_content = True
+intents = Intents.default()
+intents.typing = False
+intents.presences = False
+intents.message_content = True
 
 client = NMDownloader(
-    intents=custom_intents,
+    intents=intents,
     bot_channel=app_settings.discord.default_channel_id,
     command_prefix=app_settings.discord.command_prefix,
 )
 
+if not (token := app_settings.discord.token):
+    raise AttributeError("DISCORD_TOKEN not set")
 
-def main():
-    if not (token := app_settings.discord.token):
-        raise AttributeError("DISCORD_TOKEN not set")
-
-    client.run(token=token)
-
-
-if __name__ == "__main__":
-    main()
+client.run(token=token)
