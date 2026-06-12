a4f8a7132d8a60438831066f207d2460a0b7b507
AlexNMD <me@alex-nmd.net>
2026-06-13 00:38:35 +0200

    Add centralized Flask error handling with modular `handle_exception` utility.

--- src/config/settings.py
+++ src/config/settings.py
@@ -5,9 +5,11 @@ from pathlib import Path
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-from config.plugins.discord import DiscordConfig
-from config.plugins.un_fichier import UnFichierDownloaderConfig
-from config.plugins.youtube import YoutubeDownloaderConfig
+from config.plugins import (
+    DiscordConfig,
+    UnFichierDownloaderConfig,
+    YoutubeDownloaderConfig,
+)
 
 
 class CeleryConfig(BaseSettings):
